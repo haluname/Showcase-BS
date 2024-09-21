501cdba190feddd75d88aa9d2f7a36b8a5437967
Begovic Emanulah <ema13bego@gmail.com>
2024-09-21 21:30:12 +0200

    changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 
 window.onload = ()=>{
     ScrollReveal().reveal('header', {duration: 1000, easing: 'ease', distance: "200px", origin: "top"});
-    
 }
+
+//ScrollReveal().reveal('.images', {duration: 1000, easing: 'ease', distance: "200px", origin: "top", delay:"500"});
+
