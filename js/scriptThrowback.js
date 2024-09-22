e5c7d46af79e59cf9b0701a182466bcb3410420f
Begovic Emanulah <ema13bego@gmail.com>
2024-09-22 16:25:28 +0200

    throwback almost done

--- js/scriptThrowback.js
+++ js/scriptThrowback.js
@@ -1,18 +1,20 @@
 let showcaseData = [
     {
        "year": "1993",
-       "pic": "",
+       "pic": "mickeyMouseClub.jpg",
        "title": "The Mickey Mouse Club",
        "description": "lorem",
-       "logo": "", 
+       "logo": "britneyLogo1999.png", 
+       "index" : 1
     },
 
     {
         "year": "1998",
-        "pic": "",
+        "pic": "babyAlbum.jpg",
         "title": "...Baby One More Time release",
         "description": "lorem",
-        "logo": "", 
+        "logo": "britneyLogo1999.png", 
+        "index" : 2
      },
 
      {
@@ -21,6 +23,7 @@ let showcaseData = [
         "title": "Oops!...I Did It Again",
         "description": "lorem",
         "logo": "", 
+        "index" : 3
      },
 
     {
@@ -29,6 +32,7 @@ let showcaseData = [
         "title": "2001 VMA's",
         "description": "lorem ipsum",
         "logo": "britneyLogo2001.png", 
+        "index" : 4
      },
 
      {
@@ -37,6 +41,7 @@ let showcaseData = [
         "title": "Crossroads",
         "description": "lorem",
         "logo": "britneyLogo2002.png", 
+        "index" : 5
      },
 
      {
@@ -45,6 +50,7 @@ let showcaseData = [
         "title": "Taste The Victory",
         "description": "lorem",
         "logo": "britneyLogo2002.png", 
+        "index" : 6
      },
 
      {
@@ -53,46 +59,110 @@ let showcaseData = [
         "title": "The Onyx Hotel Tour",
         "description": "lorem",
         "logo": "britneyLogo2002.png", 
+        "index" : 7
      },
  
 
 ]
 
 
-window.onload = ()=>{
-    ScrollReveal().reveal('.yearLogo img', {duration: 1000, easing: 'ease', distance: "200px", origin: "top"});
-}
-
-document.querySelector("#changeLogo").addEventListener("click", function(){
-    let img = document.querySelector(".yearLogo img");
-    let div = document.querySelector(".yearLogo")
-    let newImg = document.createElement("img");
-    div.removeChild(img);
 
 
-    if(img.src.endsWith("britneyLogo2001.png"))
-    newImg.src = "img/britneyLogo2002.png";
-    else
-    newImg.src = "img/britneyLogo2001.png";
+// document.querySelector("#changeLogo").addEventListener("click", function(){
+//     let img = document.querySelector(".yearLogo img");
+//     let div = document.querySelector(".yearLogo")
+//     let newImg = document.createElement("img");
+//     div.removeChild(img);
+
+
+//     if(img.src.endsWith("britneyLogo2001.png"))
+//     newImg.src = "img/britneyLogo2002.png";
+//     else
+//     newImg.src = "img/britneyLogo2001.png";
 
     
-    div.appendChild(newImg);
-    ScrollReveal().reveal('.yearLogo img', {duration: 1000, easing: 'ease', distance: "200px", origin: "top"});
+//     div.appendChild(newImg);
+//     ScrollReveal().reveal('.yearLogo img', {duration: 1000, easing: 'ease', distance: "200px", origin: "top"});
 
 
-})
+// })
 
 
 
 const slider = document.querySelector('.showcaseSlider');
+
+showcaseData.forEach(data => {
+    let showcase = document.createElement("div")
+    showcase.classList.add("showcaseImage");
+    let h3 = document.createElement("h3");
+    h3.innerHTML = data.year;
+    let img = document.createElement("img")
+    img.style.width = "100px";
+    img.src = "img/" + data.pic;
+    showcase.appendChild(h3)
+    showcase.appendChild(img)
+    showcase.setAttribute('idImage', data.index);
+    slider.appendChild(showcase)
+});
+
+ScrollReveal().reveal('.background', {duration: 400, easing: 'ease'});
+    
+ScrollReveal().reveal('.yearLogo img', {duration: 1000, easing: 'ease', distance: "200px", origin: "top"});
+
+ScrollReveal().reveal('.description h4', {duration: 1000, easing: 'ease', distance: "200px", origin: "left"});
+
+ScrollReveal().reveal('.description p', {duration: 1000, easing: 'ease', distance: "200px", origin: "left", delay: 400});
+
+
+ScrollReveal().reveal('.slider-container', {duration: 1200, easing: 'ease', distance: "200px", origin: "bottom"});
+
+
+
 const showcaseImages = document.querySelectorAll('.showcaseImage');
 const prevBtn = document.getElementById('prevBtn');
 const nextBtn = document.getElementById('nextBtn');
+
+
 
 showcaseImages.forEach(image => {
     image.addEventListener("click", function(){
         showcaseImages.forEach(element=>  element.classList.remove("activeImage"))
         image.classList.add("activeImage")
+        let infoValue = image.getAttribute('idImage');
+        let info = showcaseData[infoValue-1];
+        let img = document.querySelector(".yearLogo img");
+        let div = document.querySelector(".yearLogo")
+        let newImg = document.createElement("img");
+        div.removeChild(img);
+        newImg.src = "img/" + info.logo;
+
+
+        div.appendChild(newImg);
+        ScrollReveal().reveal('.yearLogo img', {duration: 1000, easing: 'ease', distance: "200px", origin: "top"});
+
+        let background = document.querySelector(".background")
+        background.removeChild(background.querySelector("img"))
+        let newBackground = document.createElement("img");
+        newBackground.src = "img/" + info.pic;
+        background.appendChild(newBackground);
+        ScrollReveal().reveal('.background img', {duration: 400, easing: 'ease'});
+
+        let description = document.querySelector(".description")
+        description.removeChild(description.querySelector("h4"))
+        description.removeChild(description.querySelector("p"))
+        description.removeChild(description.querySelector("hr"))
+
+        let newH4 = document.createElement("h4");
+        let newP = document.createElement("p");
+        let newHR = document.createElement("hr")
+        newP.innerHTML = info.description
+        newH4.innerHTML = info.title
+
+        description.appendChild(newH4)
+        description.appendChild(newHR)
+        description.appendChild(newP)
+        ScrollReveal().reveal('.description h4', {duration: 1000, easing: 'ease', distance: "200px", origin: "left"});
+        ScrollReveal().reveal('.description p', {duration: 1000, easing: 'ease', distance: "200px", origin: "left", delay: 400});
     })
     
 });
@@ -126,3 +196,7 @@ nextBtn.addEventListener('click', () => {
 window.onload = () => {
     updateSliderPosition();
 };
+
+
+
+
